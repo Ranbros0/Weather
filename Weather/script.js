c8da89f334c5230757553137a6355df8cbc198e5
Ranbros0 <112388652+Ranbros0@users.noreply.github.com>
2024-11-12 12:23:56 +0300

    script.js

--- Weather/script.js
+++ Weather/script.js
@@ -1,4 +1,4 @@
-const apiKey = 'ВАШ_API_КЛЮЧ';  // Вставте ваш API-ключ OpenWeatherMap
+const apiKey = '80b17e2a3b849199f2311e6a41414ff9';  // Вставте ваш API-ключ OpenWeatherMap
 
 function getWeather() {
     const city = document.getElementById('city').value;
